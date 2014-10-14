fbf6310cd64993f9ae234fb0502a309653f873ad
Mathias Laurin <mathias_laurin@users.sourceforge.net>
2014-10-14 21:30:49 +0200

    Personalize pelican configuration

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,17 +17,35 @@ FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
+# Feeds
+FEEDS = (('All posts', 'feeds/all.atom.xml'),)
+
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Jinja2', 'http://jinja.pocoo.org/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('GitHub', 'https://github.com/Synss'),
+          ('LinkedIn', 'https://de.linkedin.com/in/mathiaslaurin'),)
 
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+ARTICLE_URL = "{date:%Y}/{slug}/"
+ARTICLE_SAVE_AS = "{date:%Y}/{slug}/index.html"
+
+CATEGORY_URL = "category/{slug}"
+CATEGORY_SAVE_AS = "category/{slug}/index.html"
+
+TAG_URL = "tag/{slug}"
+TAG_SAVE_AS = "tag/{slug}/index.html"
+
+# Theme
+#THEME = ""
+
+# Plugins
+PLUGIN_PATH = ""
+#PLUGINS = ["neighbors", "summary"]
--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://synss.me'
+SITEURL = 'http://blog.synss.me'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
