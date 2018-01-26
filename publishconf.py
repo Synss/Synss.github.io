9866bbc4f271d7c63d52e7bcc84e2db78b85b0f7
Mathias Laurin <mathias_laurin@users.sourceforge.net>
2018-01-26 18:25:03 +0100

    Add default `publishconf.py`

---
+++ publishconf.py
@@ -0,0 +1,10 @@
+from __future__ import unicode_literals
+import os, sys
+sys.path.append(os.curdir)
+from pelicanconf import *
+
+SITEURL = 'http://blog.synss.me'
+RELATIVE_URLS = False
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+DELETE_OUTPUT_DIRECTORY = True
