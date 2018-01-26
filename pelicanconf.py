ecf930af6592073759e244cde82b0be45ea7bb6e
Mathias Laurin <mathias_laurin@users.sourceforge.net>
2018-01-26 21:35:18 +0100

    Conf: Google tracking code

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,4 +31,5 @@ DEFAULT_PAGINATION = False
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
+GOOGLE_ANALYTICS = 'UA-56056810-1'
 DISQUS_SITENAME = 'synssblog'
