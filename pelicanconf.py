0a47905bd5194c65af073d9ad4c4f40694da6c46
Mathias Laurin <mathias_laurin@users.sourceforge.net>
2018-01-26 17:32:41 +0100

    Rerun quickstart

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,57 +2,34 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'Mathias Laurin'
-SITENAME = u"Synss' Blog"
+AUTHOR = 'Mathias Laurin'
+SITENAME = "Synss' Blog"
 SITEURL = ''
 
 PATH = 'content'
 
 TIMEZONE = 'Europe/Paris'
 
-DEFAULT_LANG = u'en'
-
-# Custom domain
-STATIC_PATHS = ["images", "extra/CNAME", "extra/robots.txt"]
-EXTRA_PATH_METADATA = {
-    "extra/CNAME": {"path": "CNAME"},
-    "extra/robots.txt": {"path": "robots.txt"},
-}
+DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
-
-# Feeds
-FEEDS = (('All posts', 'feeds/all.atom.xml'),)
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
          ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),)
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('GitHub', 'https://github.com/Synss'),
-          ('LinkedIn', 'https://de.linkedin.com/in/mathiaslaurin'),)
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-RELATIVE_URLS = True
-
-ARTICLE_URL = "{date:%Y}/{slug}/"
-ARTICLE_SAVE_AS = "{date:%Y}/{slug}/index.html"
-
-CATEGORY_URL = "category/{slug}"
-CATEGORY_SAVE_AS = "category/{slug}/index.html"
-
-TAG_URL = "tag/{slug}"
-TAG_SAVE_AS = "tag/{slug}/index.html"
-
-# Theme
-THEME = "pelican-themes/bootstrap"
-
-# Plugins
-PLUGIN_PATH = "pelican-plugins"
-PLUGINS = ["neighbors", "summary"]
+#RELATIVE_URLS = True
